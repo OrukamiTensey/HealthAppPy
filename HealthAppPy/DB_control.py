7cc215f933943ebecbf8918b0ef365cd58c646d3
Nikita Naumov <05.naumov.07.2006@gmail.com>
2025-05-25 18:36:12 +0300

    Виправлені коментарі для коректного відображення на GitHub, також додана функція для оновлення існуючих даних в таблиці бази даних

--- HealthAppPy/DB_control.py
+++ HealthAppPy/DB_control.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-# приклад коду для використання кожної функції для керування базою даних
+# sample code how to use each function to manage the database
 #
-# ініціалізуємо змінну для взаємодії з функціями керування БД
+# initialize the variable to interact with the database management functions
 #       db = DBControl()
 #
 #       db_name = "example.db"
 #       table_name = "NUTRITION"
 #
-# функція для створення файлу бази даних, для цього необхідно ввести назву
+# function to create a database file, for this you need to enter its name
 #       db.create_db(db_name)
 #
 #       sql_columns = """CREATE TABLE IF NOT EXISTS NUTRITION (
@@ -19,50 +19,65 @@ import sqlite3
 #                        KCAL INTEGER NOT NULL
 #                        );"""
 #
-# функція для створення таблиці значень в файлі бази даних, для цього зазначаємо
-# назву існуючої БД та SQL-запит з необхідною інформацією для формування таблиці
+# a function to create a table of values in a database file, for this we specify the name of
+# the existing database and the SQL query with the necessary information to form the table
 #       db.create_table(db_name, sql_columns)
 #
-# тут використовується конструкція для читання з текстового файлу рядків, які
-# містять сформовані SQL-запити, значення яких вносяться за допомогою функції
-# в базу даних
+# this uses a construct to read lines from a text file that contain generated SQL queries,
+# the values of which are entered into the database using a function
 #       with open("testfordb.txt", "r") as file:
 #           for line in file:
 #               db.insert_data(db_name, line.strip())
 #
-# приклад SQL-запитів, які вставляються: INSERT INTO NUTRITION (PRODUCT, PROTEINS, FATS, CARBOHYDRATES, KCAL) VALUES('Quince', 0.6, 0, 8.7, 37);
+# an example of inserted SQL queries: INSERT INTO NUTRITION (PRODUCT, PROTEINS, FATS, CARBOHYDRATES, KCAL) VALUES('Quince', 0.6, 0, 8.7, 37);
 #
-# функція для видалення якогось конкретного рядка за певною умовою
+# a function to delete a specific row of data in a table under a certain condition
 #       db.delete_specific_data(db_name, table_name, "PRODUCT = 'Apricot'")
 #
-# якщо потрібно просто очистити всі дані таблиці - використовуйте цю функцію
+# if you just need to clear all the data in the table, use this function
 #       db.delete_data(db_name, table_name)
 #
-# функція для друку значень з БД, за потреби можна ввести умову фільтрування значень
+# a function for printing values from the database, if necessary, you can enter a condition for filtering values
 #       db.print_data(db_name, "*", table_name)
 #       db.print_data(db_name, "*", table_name, "FATS < 9")
 #
-# функція для перевірки на наявність потрібних даних в таблиці (за умовою)
+# function to check for the presence of the required data in the table (by condition)
 #       db.data_exists(db_name, "*", table_name, "PRODUCT = 'Lamb'")
 #
-# функція для отримання всіх, відповідаючих виставленій умові, даних, у результаті
-# отримуємо масив кортежів із даними (рядки даних)
+# function to get all the data that meets the specified condition,
+# as a result, we get an array of tuples with data (data rows)
 #       result = db.receive_data(db_name, "*", table_name, "KCAL > 43")
 #
 #       for record in result:
 #           product, proteins, fats, carbohydrates, kcal = record
 #           print(product)
+#       result = db.receive_data(db_name, table_name, "*", "KCAL > 43")
+#       product_array, proteins_array, fats_array, carbohydrates_array, kcal_array = [], [], [], [], []
+#
+#       for record in result:
+#           product, proteins, fats, carbohydrates, kcal = record
+#
+#           product_array.append(product)
+#           proteins_array.append(proteins)
+#           fats_array.append(fats)
+#           carbohydrates_array.append(carbohydrates)
+#           kcal_array.append(kcal)
+#
+# a function to update the required values in the table (you need to specify the required columns and set a condition)
+#       columns = ["PRODUCT", "PROTEINS", "FATS", "CARBOHYDRATES", "KCAL"]
+#       values = ["'product'", -1, -1, -1, -1]
+#       db.update_data(db_name, table_name, columns, values, "PRODUCT = 'Lamborgini'")
 
 class DBControl:
     @staticmethod
     def create_db(file_name):
-        # Створити базу даних
+        # Create database
         conn = sqlite3.connect(file_name)
         conn.close()
 
     @staticmethod
     def create_table(file_name, sql_columns):
-        # Створити таблицю в базі даних
+        # Create table in the database
         try:
             conn = sqlite3.connect(file_name)
             cursor = conn.cursor()
@@ -76,7 +91,7 @@ class DBControl:
 
     @staticmethod
     def delete_specific_data(file_name, table_name, condition):
-        # Видалити конкретні дані з таблиці на основі умови
+        # Delete specific data (row of data) from the table, according to condition
         sql = f"DELETE FROM {table_name} WHERE {condition};"
 
         try:
@@ -102,7 +117,7 @@ class DBControl:
             
     @staticmethod
     def delete_data(file_name, table_name):
-        # Видалити всі дані з таблиці
+        # Delete all data from the table
         sql = f"DELETE FROM {table_name};"
     
         try:
@@ -129,7 +144,7 @@ class DBControl:
 
     @staticmethod
     def insert_data(file_name, sql_value):
-        # Вставити дані (один рядок) в таблицю
+        # Insert data (one row of data) into table
         try:
             conn = sqlite3.connect(file_name)
             cursor = conn.cursor()
@@ -143,7 +158,7 @@ class DBControl:
 
     @staticmethod
     def print_data(file_name, columns_name, table_name, object_condition=""):
-        # Вивести всі дані з таблиці за вказаними параметрами
+        # Print all data of the table, according to condition
         sql = f"SELECT {columns_name} FROM {table_name}"
         if object_condition:
             sql += f" WHERE {object_condition}"
@@ -163,7 +178,7 @@ class DBControl:
 
     @staticmethod
     def data_exists(file_name, columns_name, table_name, object_condition):
-        # Перевірити, чи існують дані в таблиці
+        # Check if the data exists in the table
         sql = f"SELECT {columns_name} FROM {table_name} WHERE {object_condition} LIMIT 1;"
         exists = False
 
@@ -181,7 +196,7 @@ class DBControl:
 
     @staticmethod
     def receive_data(file_name, columns_name, table_name, object_condition=""):
-        # Отримати всі дані з таблиці за вказаними параметрами
+        # Get all data from the table, according to condition
         sql = f"SELECT {columns_name} FROM {table_name}"
         if object_condition:
             sql += f" WHERE {object_condition}"
@@ -202,3 +217,28 @@ class DBControl:
             conn.close()
 
         return received_tuple
+
+    @staticmethod
+    def update_data(file_name, table_name, columns_array, values_array, object_condition=""):
+        # Update specific data in the table by setting names of columns and their values, according to condition
+        sql = f"UPDATE {table_name} SET "
+
+        for i, column in enumerate(columns_array):
+            if i != 0:
+                sql += ", "
+
+            sql += f"{column} = {values_array[i]}"
+
+        if object_condition:
+            sql += f" WHERE {object_condition}"
+
+        try:
+            conn = sqlite3.connect(file_name)
+            cursor = conn.cursor()
+            cursor.execute(sql)
+            conn.commit()
+            #print("Email updated successfully.")
+        except sqlite3.Error as e:
+            print(f"Error updating email: {e}")
+        finally:
+            conn.close()
